29ba5dd4469a4dca90c16acb94c7452b540ad5a7
kesel <a.kiselyoff@gmail.com>
2023-04-28 21:40:20 +0300

    fix some names

--- controllers/movies/add.js
+++ controllers/movies/add.js
@@ -1,6 +1,7 @@
 const Movie = require('../../models/movie');
 
 const add = async (req, res) => {
+  const { movieId } = req.params;
   const result = await Movie.create(req.body);
   res.status(201).json(result);
 };
--- controllers/movies/removeById.js
+++ controllers/movies/removeById.js
@@ -2,9 +2,9 @@ const Movie = require('../../models/movie');
 
 const removeById = async (req, res) => {
   const { movieId } = req.params;
-  const result = await Movie.findById(movieId);
+  const removedMovie = await Movie.findById(movieId);
   await Movie.findByIdAndRemove(movieId);
-  res.status(200).json({ message: 'Contact was removed', result }); // status 200 - because 204 do not return a message
+  res.status(200).json({ message: 'Movie was removed', removedMovie }); // status 200 - because 204 do not return a message
 };
 
 module.exports = removeById;
