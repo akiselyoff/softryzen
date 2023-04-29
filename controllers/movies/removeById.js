088600fad46b21e0996b0a5617f290727b39d470
kesel <a.kiselyoff@gmail.com>
2023-04-29 17:14:28 +0300

    fix date format trouble use type string and add error to the controller removeById

--- controllers/movies/removeById.js
+++ controllers/movies/removeById.js
@@ -3,6 +3,7 @@ const { Movie } = require('../../models/movie');
 const removeById = async (req, res) => {
   const { movieId } = req.params;
   const removedMovie = await Movie.findById(movieId);
+  if (!removedMovie) throw RequestError(404, 'Not found');
   await Movie.findByIdAndRemove(movieId);
   res.status(200).json({ message: 'Movie was removed', removedMovie }); // status 200 - because 204 do not return a message
 };
--- models/movie.js
+++ models/movie.js
@@ -6,7 +6,12 @@ const movieSchema = new Schema(
   {
     title: { type: String, required: true },
     director: { type: String, required: true },
-    release: { type: String, required: true },
+    release: {
+      type: String,
+      //   get: val => dateFns.format(val, 'dd-MM-yyyy'),
+      //   set: val => dateFns.parse(val, 'dd-MM-yyyy', new Date()),
+      required: true,
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -16,8 +21,7 @@ const Movie = model('movie', movieSchema);
 const movieAddSchema = Joi.object({
   title: Joi.string().required(),
   director: Joi.string().required(),
-  release: Joi.string().required(),
-  //   release: Joi.date().format('DD-MM-YYYY').less('now').required(),
+  release: Joi.date().format('DD-MM-YYYY').less('now').required(),
 });
 
 module.exports = { Movie, movieAddSchema };
