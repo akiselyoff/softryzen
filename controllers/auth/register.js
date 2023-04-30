26e5a0efe08f267222c87d07ecc37c8a6c4b0697
kesel <a.kiselyoff@gmail.com>
2023-04-30 14:08:52 +0300

    add bcrypt for password

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,8 +1,17 @@
+const bcrypt = require('bcryptjs');
 const { User } = require('../../models/user');
 const { CreateError } = require('../../helpers');
 
 const register = async (req, res) => {
   const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) throw CreateError(409, 'Email already exist');
+  const hashPassword = await bcrypt.hash(password, 10);
+  const result = await User.create({
+    email,
+    password: hashPassword,
+  });
+  res.status(201).json({ email: result.email });
 };
 
 module.exports = register;
